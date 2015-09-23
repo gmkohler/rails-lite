b0f79b5a527ea16d1105f1c4bf6dfae08dbd35d6
App Academy Student <student@appacademy.io>
2015-09-22 18:44:56 -0700

    finishing params constructor

--- lib/phase3/controller_base.rb
+++ lib/phase3/controller_base.rb
@@ -1,13 +1,22 @@
 require_relative '../phase2/controller_base'
 require 'active_support'
+require 'active_support/inflector'
 require 'active_support/core_ext'
 require 'erb'
 
 module Phase3
   class ControllerBase < Phase2::ControllerBase
+
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
+
     def render(template_name)
+      b = binding      
+      f = File.read(
+            "views/#{self.class.name.underscore}/#{template_name}.html.erb"
+          )
+      render_content(ERB.new(f).result(b), "text/html")
     end
+
   end
 end
--- lib/phase4/session.rb
+++ lib/phase4/session.rb
@@ -6,17 +6,22 @@ module Phase4
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+      app_cookie = req.cookies.find{|cook| cook.name == '_rails_lite_app'}
+      @data = app_cookie ? JSON.parse(app_cookie.value) : {}
     end
 
     def [](key)
+      @data[key]
     end
 
     def []=(key, val)
+      @data[key] = val
     end
 
     # serialize the hash into json and save in a cookie
     # add to the responses cookies
     def store_session(res)
+      res.cookies << WEBrick::Cookie.new('_rails_lite_app', @data.to_json)
     end
   end
 end
